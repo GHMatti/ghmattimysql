2936bb1b45a687e2e1b9a83f70668858f8d9d46c
iRyzor <iryzor@gmail.com>
2020-12-25 08:00:49 +0800

    Fix error: 'First argument must be a string, Buffer, ArrayBuffer, Array, or array-like object.'

--- packages/ghmattimysql/src/server/utility/getConfig.ts
+++ packages/ghmattimysql/src/server/utility/getConfig.ts
@@ -12,11 +12,14 @@ function getConfigFromConnectionString() {
       .replace(/(?:user\s?(?:id|name)?|uid)=/gi, 'user=')
       .replace(/(?:password|pwd)=/gi, 'password=')
       .replace(/(?:database|initial\scatalog)=/gi, 'database=');
-    connectionStr.split(';').forEach((el) => {
-      const equal = el.indexOf('=');
-      const key = (equal > -1) ? el.substr(0, equal) : el;
-      const value = (equal > -1) ? el.substr(equal + 1) : '';
-      cfg[key.trim()] = (Number.isNaN(Number(value))) ? value : Number(value);
+    connectionStr.split(';').forEach(c => {
+      const equal = c.indexOf('=');
+      const key = (equal > -1) ? c.substr(0, equal) : c;
+      const value = (equal > -1) ? c.substr(equal + 1) : '';
+      cfg = {
+        ...cfg,
+        [key]: key.includes("port") ? Number(value) : value
+      };
     });
   } else if (/mysql:\/\//gi.test(connectionString)) {
     cfg = parseUrl(connectionString);
